9bc59c85857f2ee2d98d65ef67eb3eccea95baa7
Ruslan <ruslan.rekunov@yandex.ru>
2022-02-21 08:48:58 +0300

    v2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void) {
+    char A;
+  printf ("Введите число", A);
+  scanf ("\n%с", &A);
+    char B;
+  printf ("Введите число", B);
+  scanf ("\n%с", &B);
+    char c;
+    c = A+B;
+    printf ("\nСумма чисел равна : %c \r\n",c);
+   scanf ("\n%с", &c);
+    return 0;
+}
