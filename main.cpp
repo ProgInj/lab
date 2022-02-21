5c1ca5b71400c2c934a2df557dbe9de05d7c8874
Ruslan <ruslan.rekunov@yandex.ru>
2022-02-21 08:51:36 +0300

    v3

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main (void) {
-    char A;
-  printf ("Введите число", A);
-  scanf ("\n%с", &A);
-    char B;
-  printf ("Введите число", B);
-  scanf ("\n%с", &B);
-    char c;
-    c = A+B;
-    printf ("\nСумма чисел равна : %c \r\n",c);
-   scanf ("\n%с", &c);
-    return 0;
-}
+#include <iostream>
+#include <sdt>
+int main()
+{
+setlocale(LC_ALL, "Russia");
+int x, y;
+cout « "введите x: ";
+cin » x;
+cout « "введите y: ";
+cin » y;
+cout « " x + y = " « x + y « endl;
+return 0;
